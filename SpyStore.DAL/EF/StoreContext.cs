8a594b4a69efd0b27360081b47aaacb1f125e117
Chris Sanders <SandersC22@lsus.edu>
2018-03-27 01:10:20 -0600

    Assignment 6 commit

---
+++ SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=laptop-vsk60isa\sqlexpress;Database=SpyStoreDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;", options => options.EnableRetryOnFailure());
+            }
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ SpyStore.Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.Models
+{
+    class Class1
+    {
+        static void Main(string[] args) { }
+    }
+}
